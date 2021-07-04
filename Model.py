1984e618a293e482fae0bcab719eb61de8a0def2
Evgenii Ryabenkov <EARyabenkov@sberbank.ru>
2021-07-05 02:46:16 +0600

    WIP some result

--- Model.py
+++ Model.py
@@ -1,4 +1,5 @@
 from typing import Any, Dict
+import random
 
 import config
 import psycopg2
@@ -19,52 +20,57 @@ class DBConfigSettings(BaseDBSettings):
         self.db_name = config.DB_NAME
 
 
-class Driver:
-    def __init__(self, db_setting: DBConfigSettings):
-        self.setting = db_setting
+class Driver(DBConfigSettings):
+    def __init__(self):
+        super().__init__()
         self.connection = psycopg2.connect(
-            f"dbname={self.setting.db_name} user={self.setting.user} password={self.setting.password}")
+            f"dbname={self.db_name} user={self.user} password={self.password}")
         self.__cursor = self.connection.cursor()
 
     def __del__(self, instance):
         self.connection.close()
 
 
-class Model:
+class Model(Driver):
+    _table: str
 
-    def __init__(self, db_driver: Driver):
-        self.__cursor = db_driver.connection.cursor()
+    _instance = None
 
-    id: int
-    __TABLE_NAME = str
+    def __new__(cls, *args, **kwargs):
+        if not isinstance(cls._instance, cls):
+            cls._instance = object.__new__(cls, *args, **kwargs)
+        return cls._instance
 
-    def create(self, **args) -> Dict:
+    def create(self, *args) -> Dict:
         data_array = []
-        print(args)
-        keys = ', '.join([key for key in args.keys()])
-        values = ', '.join([str(val) for val in args.values()])
-        query = f"INSERT INTO {config.DB_NAME}.{self.__TABLE_NAME} {keys} VALUES "
-        data_array.append(f"{values}")
+
+        # keys = ', '.join([key for key in args])
+        # values = ', '.join([val for val in self])
+        a4 = ("Model.__dict__:", self.__dict__)
+        values = 'bar'
+        query = f"INSERT INTO {config.DB_NAME}.{self._table} {args} VALUES "
+        # data_array.append(f"{values}")
         sql_query = query + ", ".join(data_array) + ';'
+        # print(sql_query)
         return self.__cursor.execute(sql_query)
 
-    def read(self, **kvargs) -> Dict:
-        key, value = kvargs
-        data_array = []
-        query = f"SELECT * FROM {config.DB_NAME}.{self.__TABLE_NAME} WHERE {key} = {value};"
-        return self.__cursor.execute(query)
+    # def read(self, **kvargs) -> Dict:
+    #     key, value = kvargs
+    #     data_array = []
+    #     query = f"SELECT * FROM {config.DB_NAME}.{self.__table} WHERE {key} = {value};"
+    #     return self.__cursor.execute(query)
 
-    def update(self):
-        pass
-
-    def delete(self):
-        pass
-
-    def create_table(self):
-        pass
-
-    def delete_table(self):
-        pass
-
-    def alter_table(self):
-        pass
+    # def update(self):
+    #     pass
+    #
+    # def delete(self):
+    #     pass
+    #
+    # def create_table(self):
+    #     pass
+    #
+    # def delete_table(self):
+    #     pass
+    #
+    # def alter_table(self):
+    #     pass
--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-from Model import Driver, DBConfigSettings
 from models.Article import Article
+from models.Media_types import MediaType
 
 
 # @route("POST", "/new_article")
-def new_article(flow):
-    new_article = Article(Driver(DBConfigSettings()))
+def new_article():
+    flow = {'id': 1, 'title': 'zopa', 'description': 'foo', 'content': 'FOO'}
+    new_article_ = Article()
+    new_article_.id = flow['id']
+    new_article_.title = flow['title']
+    new_article_.description = flow['description']
+    new_article_.content = flow['content']
 
-    new_article.title = flow['title']
-    new_article.description = flow['description']
-    new_article.content = flow['content']
+    keys = f"({', '.join([key for key in flow.keys()])})"
+    new_article_.create(keys)
 
-    new_article.create()
+
+def new_media_type(flow):
+    new_media_type = MediaType()
+    new_media_type.title = flow['title']
+    new_media_type.description = flow['description']
+    new_media_type.content = flow['content']
+    new_media_type.create()
 
 
 if __name__ == '__main__':
-    flow = {'title': 'zopa', 'description': 'foo', 'content': 'FOO'}
-    new_article(flow)
+    
+    new_article()
