370e9b99577a11ca8184fe0be46a6ff89c74709a
Evgenii Ryabenkov <EARyabenkov@sberbank.ru>
2021-07-08 20:15:50 +0600

    Ready dynamic equal condition

--- Conditions.py
+++ Conditions.py
@@ -1,21 +1,28 @@
 # -*- coding: utf-8 -*-
-from typing import Any, List
+from typing import Any
 
 
 class QFilter:
-    prepare_condition: List
-
     def __init__(self, *args):
+        self.prepare_conditions = []
+        self.and_clause_str: str
         self.condition = args
+        for elem in self.condition:
+            if type(elem) is EQ:
+                self.prepare_conditions.append(elem.prepare_condition())
+        self.and_clause_str = ' AND '.join(self.prepare_conditions)
 
-    def __str__(self):
-        return f'WHERE {self.condition if len(self.condition) > 1 else self.condition[0]}'
+    def __repr__(self):
+        return self.and_clause_str
 
 
 class EQ:
-    def __init__(self, first_arg: str, second_arg: Any):
-        self.first_arg = first_arg
-        self.second_arg = second_arg
+    def __init__(self, key_arg: str, value_arg: Any):
+        self.key_arg = key_arg
+        self.value_arg = value_arg
 
-    def __str__(self):
-        return f"{self.first_arg} = {self.second_arg}"
+    def prepare_condition(self):
+        if type(self.value_arg) is str:
+            return "%s = '%s'" % (self.key_arg, self.value_arg)
+        else:
+            return "%s = %s" % (self.key_arg, self.value_arg)
